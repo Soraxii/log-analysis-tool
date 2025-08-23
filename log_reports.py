c82994bee1526ad4917dd22f64d09945ca205dae
Amr <amrahmed2005i@gmail.com>
2025-08-23 19:45:20 +0300

    added try except lines to soft crash if any unexpected errors happen

--- log_reports.py
+++ log_reports.py
@@ -6,81 +6,84 @@ import sys
 
 def log_parsing(file):
     ''' takes the log file as input and parses through it then adds it to a dictionary'''
-    with open(file, 'r') as logs:
+    try:
 
-        data={
-            'category':{},
-            'time':[],
-            'failed_users':{},
-            'success_log':{},
-            'warnings':[],
-            'num_events':0
-        }
-    #this pattern searches for the timestamps and codes
-        pattern= r'^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) ([A-Z]+): '
+        with open(file, 'r') as logs:
 
-        for line in logs:
+            data={
+                'category':{},
+                'time':[],
+                'failed_users':{},
+                'success_log':{},
+                'warnings':[],
+                'num_events':0
+            }
+        #this pattern searches for the timestamps and codes
+            pattern= r'^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) ([A-Z]+): '
 
-            data['num_events']+=1
-            
-            match= re.search(pattern,line.strip())
+            for line in logs:
 
-            if match:
-
-                timestamp= match.group(1)
-                code= match.group(2)
-
-                data['time'].append(timestamp)
+                data['num_events']+=1
                 
+                match= re.search(pattern,line.strip())
 
-                if code not in data['category'].keys():
+                if match:
 
-                    data['category'][code]=1
+                    timestamp= match.group(1)
+                    code= match.group(2)
 
-                else:
+                    data['time'].append(timestamp)
+                    
 
-                    data['category'][code]+=1
+                    if code not in data['category'].keys():
 
-    #this pattern will search for failed login attempts and record the users and thier IP address
-
-                failed_match = re.search(r'Failed login attempt for user \'([a-zA-Z0-9]*)\' from IP (\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})$',line)
-                if failed_match:
-                    failed_user=failed_match.group(1)
-                    failed_ip=failed_match.group(2)
-
-                    if failed_user not in data['failed_users'].keys():
-
-                        data['failed_users'][failed_user]=failed_ip
-
-    # this pattern will show the users that successfully logged in
-                users_logged = re.search(r'User \'([a-zA-Z0-9]*)\' logged in from IP (\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})$',line)
-
-                if users_logged:
-
-                    success_user = users_logged.group(1)
-                    logged_ip = users_logged.group(2)
-
-                    if success_user not in data['success_log'].keys():
-
-                        data['success_log'][success_user]=logged_ip
+                        data['category'][code]=1
 
                     else:
 
-                        continue
+                        data['category'][code]+=1
 
+        #this pattern will search for failed login attempts and record the users and thier IP address
 
+                    failed_match = re.search(r'Failed login attempt for user \'([a-zA-Z0-9]*)\' from IP (\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})$',line)
+                    if failed_match:
+                        failed_user=failed_match.group(1)
+                        failed_ip=failed_match.group(2)
 
-    #this pattern will match for any warning and store it
+                        if failed_user not in data['failed_users'].keys():
 
-                syswarn = re.search(r'WARNING: ([\w\s\d\%\/\(\)]*)',line.strip())
+                            data['failed_users'][failed_user]=failed_ip
 
-                if syswarn:
+        # this pattern will show the users that successfully logged in
+                    users_logged = re.search(r'User \'([a-zA-Z0-9]*)\' logged in from IP (\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})$',line)
 
-                    data['warnings'].append(syswarn.group(1))
+                    if users_logged:
+
+                        success_user = users_logged.group(1)
+                        logged_ip = users_logged.group(2)
+
+                        if success_user not in data['success_log'].keys():
+
+                            data['success_log'][success_user]=logged_ip
+
+                        else:
+
+                            continue
+
+        #this pattern will match for any warning and store it
+
+                    syswarn = re.search(r'WARNING: ([\w\s\d\%\/\(\)]*)',line.strip())
+
+                    if syswarn:
+
+                        data['warnings'].append(syswarn.group(1))
+    except Exception as e:
+        print(f"Error occured while parsing log file: {e}")
     return data 
             
 def generate_report(data):  
-        '''generates a report based on the data parsed from the log file'''
+    '''generates a report based on the data parsed from the log file'''
+    try: 
 
         print("=== Log Analysis Report === ")
         print(f'Time Range: {data['time'][0]} to {data['time'][-1]}')
@@ -103,7 +106,9 @@ def generate_report(data):
         for i in range(len(data['warnings'])):
 
             print(f'{i+1}. {data['warnings'][i]}')
-    
+
+    except Exception as e:
+        print(f"Error occured while generating the report: {e}")
 
 
 def main():
